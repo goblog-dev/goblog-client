edc691184689d798467a4160207c901ddfee41aa
Michael Putong <michael.wenceslaus@gmail.com>
2024-06-11 22:07:16 +0700

    modified:   src/pages/article/index.tsx         modified:   src/pages/category/index.tsx

--- src/pages/article/index.tsx
+++ src/pages/article/index.tsx
@@ -3,15 +3,16 @@
 import ArticleList from "@/pages/article/articleList";
 import ArticleContent from "@/pages/article/articleContent";
 import useSWR from 'swr';
-import {Spin} from "@/components/spin";
-import {useMemo, useState} from "react";
+import {useContext, useMemo, useState} from "react";
 import {Article as articleInterface} from "@/models/articles";
 import {Alert} from "@/components/alert";
+import {CommonContext} from "@/app/commonContext";
 
 const fetcher: any = (url: string) => fetch(url).then((res) => res.json());
 
 const Article = () => {
-    const { data, error, isLoading } = useSWR("/api/v1/articles", fetcher);
+    const {data, error, isLoading} = useSWR("/api/v1/articles", fetcher);
+    const {setIsGlobalLoading} = useContext(CommonContext);
     const [currentArticle, setCurrentArticle] = useState<articleInterface>({
         category_id: 0,
         category_name: "",
@@ -30,13 +31,14 @@ const Article = () => {
         return currentArticle;
     }, [currentArticle])
 
-    if (error) return (<Alert type="error" title={'Article'} message={data ? data.translate : "cannot reach server"} />);
-    if (isLoading) return (<Spin />)
+    if (error) return (<Alert type="error" title={'Article'} message={data ? data.translate : "cannot reach server"}/>);
+    if (isLoading) return (setIsGlobalLoading(true));
+    if (data) setIsGlobalLoading(false);
 
     return (
         <div className="flex flex-row translate-y-2 w-10/12 space-x-20">
             <div className="left-0 w-fit border-r border-gray-200">
-                <ArticleList data={data.data} setCurrentArticle={setCurrentArticle} />
+                <ArticleList data={data.data} setCurrentArticle={setCurrentArticle}/>
             </div>
             <div className="p-3 w-full">
                 <ArticleContent data={currentArticleMemo}/>
--- src/pages/category/index.tsx
+++ src/pages/category/index.tsx
@@ -5,15 +5,19 @@ import {Spin} from "@/components/spin";
 import {Dropdown, DropdownStruct} from "@/components/dropdown";
 import {Category as categoryStruct} from "@/models/categories";
 import {Alert} from "@/components/alert";
+import {useContext} from "react";
+import {CommonContext} from "@/app/commonContext";
 
 
 const fetcher: any = (url: string) => fetch(url).then((res) => res.json());
 
 const Category = (props: any) => {
     const {data, error, isLoading} = useSWR(`/api/v1/categories`, fetcher);
+    const {setIsGlobalLoading} = useContext(CommonContext);
 
     if (error) return (<Alert type="error" title={'Article'} message={data ? data.translate : "cannot reach server"}/>);
-    if (isLoading) return (<Spin/>)
+    if (isLoading) return (setIsGlobalLoading(true))
+    if (data) setIsGlobalLoading(false);
 
     if (data.data == undefined) {
         return <Dropdown data={[]} />
